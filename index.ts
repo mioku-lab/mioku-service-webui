7a7e19df4239cd0f22b9ae454a5b5bf3cc242a11
Jerryplusy <Jerryplusy@outlook.com>
2026-04-23 18:42:56 +0800

    chore: update version

--- index.ts
+++ index.ts
@@ -207,7 +207,7 @@ const runtime = new WebUIRuntime();
 
 const webUIService: MiokuService = {
   name: "webui",
-  version: "1.8.0",
+  version: "1.9.0",
   description: "Mioku WebUI 管理服务",
   api: {
     getSettings: () => getWebUISettings(),
